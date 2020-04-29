f61c780e346e06e02ac0c404c59cc6410ee21559
obsq <63944348+obsq@users.noreply.github.com>
2020-04-29 11:30:52 +0530

    Create generate_session_file.py

---
+++ generate_session_file.py
@@ -0,0 +1,13 @@
+# This script wont run your bot, it just generates a session.
+
+from telethon import TelegramClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv("config.env")
+
+API_KEY = os.environ.get("API_KEY", None)
+API_HASH = os.environ.get("API_HASH", None)
+
+bot = TelegramClient('userbot', API_KEY, API_HASH)
+bot.start()
