c4a51e097e973b331dc3aad8c43460f88304b362
obsq <63944348+obsq@users.noreply.github.com>
2020-04-29 12:22:15 +0530

    Create __main__.py

---
+++ userbot/__main__.py
@@ -0,0 +1,69 @@
+from userbot import bot
+from sys import argv
+import sys
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
+from telethon import TelegramClient
+from var import Var
+from userbot.utils import load_module
+from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
+from pathlib import Path
+import asyncio
+import telethon.utils
+from userbot.events import  remove_plugin as rp
+from userbot.events import  load_module as lm
+from userbot import bot    
+from userbot.events import kyne3301, obsq, qsbo
+borg = bot
+admin_cmd = obsq
+command = qsbo
+register = kyne3301
+remove_plugin = lm
+load_module = rp
+
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+
+
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished with no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
+    
+
+import glob
+path = 'userbot/modules/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
+
+import userbot._core
+from userbot.modules import api, beta, extra, fun, group, help, person, stickers, system
+
+LOGS.info("Congratulations, javes is now running !!\
+          \nTest it by typing !javes in any chat.")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
