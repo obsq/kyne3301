755ff11453152cd00c5d7bfe5835bf6833a95f15
obsq <63944348+obsq@users.noreply.github.com>
2020-04-29 14:37:33 +0530

    Create utils.py

---
+++ userbot/utils.py
@@ -0,0 +1,10 @@
+from userbot.events import  remove_plugin as rp
+from userbot.events import  load_module as lm
+from userbot import bot    
+from userbot.events import kyne3301, obsq, qsbo
+borg = bot
+admin_cmd = obsq
+command = qsbo
+register = kyne3301
+remove_plugin = lm
+load_module = rp
