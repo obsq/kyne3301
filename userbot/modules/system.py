5beac598d90377e0137d9261b8f7eef0de833abe
obsq <63944348+obsq@users.noreply.github.com>
2020-04-29 14:51:44 +0530

    Create system.py

---
+++ userbot/modules/system.py
@@ -0,0 +1,227 @@
+from datetime import datetime
+from random import randint
+from asyncio import sleep
+from os import execl
+import sys
+import os
+from userbot import bot
+from userbot.events import admin_cmd, command
+import io
+import heroku3
+from userbot import CMD_HELP, ALIVE_NAME, PM_MESSAGE, KYNE_NAME, KYNE_MSG, ORI_MSG
+import asyncio
+from asyncio import create_subprocess_shell as asyncSubprocess
+from asyncio.subprocess import PIPE as asyncPIPE
+from userbot import CMD_HELP, LOGS, HEROKU_APPNAME, HEROKU_APIKEY
+import sys
+import json
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, UPSTREAM_REPO_URL
+from speedtest import Speedtest
+from telethon import functions
+from os import remove, execle, path, makedirs, getenv, environ
+from shutil import rmtree
+import asyncio
+import json
+from asyncio import sleep
+from telethon.errors import rpcbaseerrors
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+import os
+import subprocess
+import time
+import math
+from pySmartDL import SmartDL
+import asyncio
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from telethon.tl.types import DocumentAttributeVideo
+from userbot import LOGS, CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
+import sys
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
+from userbot import CMD_HELP, bot, HEROKU_APIKEY, HEROKU_APPNAME, UPSTREAM_REPO_URL
+from userbot import CMD_HELP, LOGS, HEROKU_APP_NAME, HEROKU_API_KEY
+from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+from platform import python_version, uname
+from shutil import which
+from os import remove
+from telethon import version
+from userbot import CMD_HELP, ALIVE_NAME
+from userbot.events import javes05
+import heroku3
+import asyncio
+import os
+import requests
+import math
+
+from userbot import CMD_HELP, HEROKU_APP_NAME, HEROKU_API_KEY
+from userbot.events import kyne3301
+from userbot.prettyjson import prettyjson
+from userbot import CMD_HELP, ALIVE_NAME, PM_MESSAGE, KYNE_NAME, KYNE_MSG, ORI_MSG, ALIVE_S_MESSAGE, ALIVE_E_MESSAGE, ALIVE_S_MSG, ALIVE_E_MSG
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+
+
+# ================= CONSTANT =================
+KYNE_NNAME = str(KYNE_NAME) if KYNE_NAME else str(KYNE_MSG)
+ALIVE_S_MMSG = str(ALIVE_S_MESSAGE) if ALIVE_S_MESSAGE else str(ALIVE_S_MSG)
+ALIVE_E_MMSG = str(ALIVE_E_MESSAGE) if ALIVE_E_MESSAGE else str(ALIVE_E_MSG)
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+
+
+async def subprocess_run(cmd, heroku):
+    subproc = await asyncSubprocess(cmd, stdout=asyncPIPE, stderr=asyncPIPE)
+    stdout, stderr = await subproc.communicate()
+    exitCode = subproc.returncode
+    if exitCode != 0:
+        await heroku.edit(
+            '**An error was detected while running subprocess**\n'
+            f'```exitCode: {exitCode}\n'
+            f'stdout: {stdout.decode().strip()}\n'
+            f'stderr: {stderr.decode().strip()}```')
+        return exitCode
+    return stdout.decode().strip(), stderr.decode().strip(), exitCode
+
+
+
+@kyne3301(outgoing=True, pattern=r"^!heroku(?: |$)(.*)")
+async def heroku_manager(heroku):
+    await heroku.edit("`Processing...`")
+    await asyncio.sleep(3)
+    conf = heroku.pattern_match.group(1)
+    result = await subprocess_run(f'heroku ps -a {HEROKU_APP_NAME}', heroku)
+    if result[2] != 0:
+        return
+    hours_remaining = result[0]
+    await heroku.edit('`' + hours_remaining + '`')
+    return
+
+
+
+@kyne3301(outgoing=True, pattern="^\!sysd$")
+async def sysdetails(sysd):
+    """ For .sysd command, get system info using neofetch. """
+    try:
+        neo = "neofetch --stdout"
+        fetch = await asyncrunapp(
+            neo,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+
+        stdout, stderr = await fetch.communicate()
+        result = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        await sysd.edit("`" + result + "`")
+    except FileNotFoundError:
+        await sysd.edit("`Install neofetch first !!`")
+
+
+
+
+
+@kyne3301(outgoing=True, pattern="^\!pip(?: |$)(.*)")
+async def pipcheck(pip):
+    """ For .pip command, do a pip search. """
+    pipmodule = pip.pattern_match.group(1)
+    if pipmodule:
+        await pip.edit("`Searching . . .`")
+        invokepip = f"pip3 search {pipmodule}"
+        pipc = await asyncrunapp(
+            invokepip,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+
+        stdout, stderr = await pipc.communicate()
+        pipout = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        if pipout:
+            if len(pipout) > 4096:
+                await pip.edit("`Output too large, sending as file`")
+                file = open("output.txt", "w+")
+                file.write(pipout)
+                file.close()
+                await pip.client.send_file(
+                    pip.chat_id,
+                    "output.txt",
+                    reply_to=pip.id,
+                )
+                remove("output.txt")
+                return
+            await pip.edit("**Query: **\n`"
+                           f"{invokepip}"
+                           "`\n**Result: **\n`"
+                           f"{pipout}"
+                           "`")
+        else:
+            await pip.edit("**Query: **\n`"
+                           f"{invokepip}"
+                           "`\n**Result: **\n`No Result Returned/False`")
+    else:
+        await pip.edit("`Use .help pip to see an example`")
+
+
+@kyne3301(outgoing=True, pattern="^\!kyne$")
+async def amireallyalive(alive):
+    """ For .kyne command, check if the bot is running.  """
+    await alive.edit(""
+                    f" **{ALIVE_S_MMSG}**\n" 
+                    f"---------------------------\n"
+                    f"  >`{KYNE_NNAME}`: ** 2.5.1**\n"
+                    f"  >`Telethon`: ** {version.__version__} **\n"
+                    f"  >`Python` : ** {python_version()} **\n"
+                    f"  >`User:` ** {DEFAULTUSER} **\n"
+                    f"---------------------------\n"
+                    f" **{ALIVE_E_MMSG}**")
+
+
+
+
+@bot.on(admin_cmd(pattern=f"sudo$", allow_sudo=True))
+async def iqless(e):
+    await e.reply(""
+                    f" **Hello Sir Iam Alive!**\n" 
+                    f"---------------------------\n"
+                    f"  >`{KYNE_NNAME}`: ** 2.5.1**\n"
+                    f"  >`Telethon`: ** {version.__version__} **\n"
+                    f"  >`Python` : ** {python_version()} **\n"
+                    f"  >`User:` ** Sudo **\n"
+                    f"---------------------------")
+
+
+
+
+
+
+
+
+requirements_path = path.join(
+    path.dirname(path.dirname(path.dirname(__file__))), 'requirements.txt')
+
+
+async def gen_chlog(repo, diff):
+    ch_log = ''
+    d_form = "%d/%m/%y"
+    for c in repo.iter_commits(diff):
+        ch_log += f'•[{c.committed_datetime.strftime(d_form)}]: {c.summary} <{c.author}>\n'
+    return ch_log
+
+
+async def update_requirements():
+    reqs = str(requirements_path)
+    try:
+        process = await asyncio.create_subprocess_shell(
+            ' '.join([sys.executable, "-m", "pip", "install", "-r", reqs]),
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        await process.communicate()
+        return process.returncode
+    except Exception as e:
+        return repr(e)
+
