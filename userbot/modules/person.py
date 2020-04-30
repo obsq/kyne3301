d8ed6ed5964613daf2f160b46c4726c8ba4398cc
obsq <63944348+obsq@users.noreply.github.com>
2020-04-30 19:17:12 +0530

    Update person.py

--- userbot/modules/person.py
+++ userbot/modules/person.py
@@ -605,3 +605,201 @@ async def who(event):
     replied_user = await get_user(event)
 
     try:
+        photo, caption = await fetch_info(replied_user, event)
+    except AttributeError:
+        event.edit("`Could not fetch info of that user.`")
+        return
+
+    message_id_to_reply = event.message.reply_to_msg_id
+
+    if not message_id_to_reply:
+        message_id_to_reply = None
+
+    try:
+        await event.client.send_file(event.chat_id,
+                                     photo,
+                                     caption=caption,
+                                     link_preview=False,
+                                     force_document=False,
+                                     reply_to=message_id_to_reply,
+                                     parse_mode="html")
+
+        if not photo.startswith("http"):
+            os.remove(photo)
+        await event.delete()
+
+    except TypeError:
+        await event.edit(caption, parse_mode="html")
+
+
+async def get_user(event):
+    """ Get the user from argument or replied message. """
+    if event.reply_to_msg_id and not event.pattern_match.group(1):
+        previous_message = await event.get_reply_message()
+        replied_user = await event.client(
+            GetFullUserRequest(previous_message.from_id))
+    else:
+        user = event.pattern_match.group(1)
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+
+        if event.message.entities is not None:
+            probable_user_mention_entity = event.message.entities[0]
+
+            if isinstance(probable_user_mention_entity,
+                          MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user
+        try:
+            user_object = await event.client.get_entity(user)
+            replied_user = await event.client(
+                GetFullUserRequest(user_object.id))
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+
+    return replied_user
+
+
+async def fetch_info(replied_user, event):
+    """ Get details from the User object. """
+    replied_user_profile_photos = await event.client(
+        GetUserPhotosRequest(user_id=replied_user.user.id,
+                             offset=42,
+                             max_id=0,
+                             limit=80))
+    replied_user_profile_photos_count = "Person needs help with uploading profile picture."
+    try:
+        replied_user_profile_photos_count = replied_user_profile_photos.count
+    except AttributeError as e:
+        pass
+    user_id = replied_user.user.id
+    first_name = replied_user.user.first_name
+    last_name = replied_user.user.last_name
+    try:
+        dc_id, location = get_input_location(replied_user.profile_photo)
+    except Exception as e:
+        dc_id = "Couldn't fetch DC ID!"
+        location = str(e)
+    common_chat = replied_user.common_chats_count
+    username = replied_user.user.username
+    user_bio = replied_user.about
+    is_bot = replied_user.user.bot
+    restricted = replied_user.user.restricted
+    verified = replied_user.user.verified
+    photo = await event.client.download_profile_photo(user_id,
+                                                      TEMP_DOWNLOAD_DIRECTORY +
+                                                      str(user_id) + ".jpg",
+                                                      download_big=True)
+    first_name = first_name.replace(
+        "\u2060", "") if first_name else ("This User has no First Name")
+    last_name = last_name.replace(
+        "\u2060", "") if last_name else ("This User has no Last Name")
+    username = "@{}".format(username) if username else (
+        "This User has no Username")
+    user_bio = "This User has no About" if not user_bio else user_bio
+
+    caption = "<b>USER INFO:</b>\n\n"
+    caption += f"First Name: {first_name}\n"
+    caption += f"Last Name: {last_name}\n"
+    caption += f"Username: {username}\n"
+    caption += f"Data Centre ID: {dc_id}\n"
+    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
+    caption += f"Is Bot: {is_bot}\n"
+    caption += f"Is Restricted: {restricted}\n"
+    caption += f"Is Verified by Telegram: {verified}\n"
+    caption += f"ID: <code>{user_id}</code>\n\n"
+    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
+    caption += f"Common Chats with this user: {common_chat}\n"
+    caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
+    
+    return photo, caption
+
+
+
+@kyne3301(outgoing=True, pattern="^\!purge$")
+async def fastpurger(purg):
+    """ For .purge command, purge all messages starting from the reply. """
+    chat = await purg.get_input_chat()
+    msgs = []
+    itermsg = purg.client.iter_messages(chat, min_id=purg.reply_to_msg_id)
+    count = 0
+
+    if purg.reply_to_msg_id is not None:
+        async for msg in itermsg:
+            msgs.append(msg)
+            count = count + 1
+            msgs.append(purg.reply_to_msg_id)
+            if len(msgs) == 100:
+                await purg.client.delete_messages(chat, msgs)
+                msgs = []
+    else:
+        await purg.edit("`I need a mesasge to start purging from.`")
+        return
+
+    if msgs:
+        await purg.client.delete_messages(chat, msgs)
+    done = await purg.client.send_message(
+        purg.chat_id, f"`Fast purge complete!`\
+        \nPurged {str(count)} messages")
+
+    if BOTLOG:
+        await purg.client.send_message(
+            BOTLOG_CHATID,
+            "Purge of " + str(count) + " messages done successfully.")
+    await sleep(2)
+    await done.delete()
+
+
+@kyne3301(outgoing=True, pattern="^\!purgeme")
+async def purgeme(delme):
+    """ For .purgeme, delete x count of your latest message."""
+    message = delme.text
+    count = int(message[9:])
+    i = 1
+
+    async for message in delme.client.iter_messages(delme.chat_id,
+                                                    from_user='me'):
+        if i > count + 1:
+            break
+        i = i + 1
+        await message.delete()
+
+    smsg = await delme.client.send_message(
+        delme.chat_id,
+        "`Purge complete!` Purged " + str(count) + " messages.",
+    )
+    if BOTLOG:
+        await delme.client.send_message(
+            BOTLOG_CHATID,
+            "Purge of " + str(count) + " messages done successfully.")
+    await sleep(2)
+    i = 1
+    await smsg.delete()
+
+
+@kyne(outgoing=True, pattern="^\!del$")
+async def delete_it(delme):
+    """ For .del command, delete the replied message. """
+    msg_src = await delme.get_reply_message()
+    if delme.reply_to_msg_id:
+        try:
+            await msg_src.delete()
+            await delme.delete()
+            if BOTLOG:
+                await delme.client.send_message(
+                    BOTLOG_CHATID, "Deletion of message was successful")
+        except rpcbaseerrors.BadRequestError:
+            if BOTLOG:
+                await delme.client.send_message(
+                    BOTLOG_CHATID, "Well, I can't delete a message")
+
+
+@kyne3301(outgoing=True, pattern="^\!edit")
+async def editer(edit):
