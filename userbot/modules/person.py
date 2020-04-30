f47832121f8357206df96ef760189f916e501e5f
obsq <63944348+obsq@users.noreply.github.com>
2020-04-30 18:50:18 +0530

    Update person.py

--- userbot/modules/person.py
+++ userbot/modules/person.py
@@ -1,1 +1,251 @@
 
+from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
+from telethon.tl.functions.messages import ReportSpamRequest
+from telethon.tl.types import User
+from datetime import datetime
+from asyncio import sleep
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from random import choice, randint
+from asyncio import sleep
+import asyncio
+from telethon import events
+
+from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.events import StopPropagation
+import asyncio
+from telethon import events
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot import (AFKREASON, COUNT_MSG, CMD_HELP, ISAFK, BOTLOG,
+                     BOTLOG_CHATID, USERS, PM_AUTO_BAN)
+from userbot.events import kyne3301
+from userbot import bot, CMD_HELP
+from telethon.errors import rpcbaseerrors
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+
+import os
+
+from userbot import CMD_HELP, BOTLOG_CHATID
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
+from speedtest import Speedtest
+from telethon import functions
+from os import remove, execle, path, makedirs, getenv, environ
+from shutil import rmtree
+import asyncio
+import json
+from random import choice, randint
+from asyncio import sleep
+from telethon.events import StopPropagation
+from userbot import (AFKREASON, COUNT_MSG, CMD_HELP, ISAFK, BOTLOG,BOTLOG_CHATID, USERS, PM_AUTO_BAN)
+
+from asyncio import sleep
+from telethon.errors import rpcbaseerrors
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+
+import os
+import subprocess
+import time
+import math
+from pySmartDL import SmartDL
+import asyncio
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from telethon.tl.types import DocumentAttributeVideo
+from userbot import LOGS, CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
+import sys
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
+from userbot import CMD_HELP, bot, HEROKU_APIKEY, HEROKU_APPNAME, UPSTREAM_REPO_URL
+
+from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+from platform import python_version, uname
+from shutil import which
+from os import remove
+from telethon import version
+from userbot import CMD_HELP, ALIVE_NAME, PM_MESSAGE, KYNE_NAME, KYNE_MSG, ORI_MSG, AFK_MESSAGE, AFK_MSG, BLOCK_MSG, BLOCK_MESSAGE
+from datetime import datetime
+import time
+from random import choice, randint
+from asyncio import sleep
+
+
+import pytz
+import math
+import time as t
+x = math.inf
+counter = 0
+start=t.time()
+
+
+
+from telethon.events import StopPropagation
+
+from userbot import (AFKREASON, COUNT_MSG, CMD_HELP, ISAFK, BOTLOG,
+                     BOTLOG_CHATID, USERS, PM_AUTO_BAN)
+from sqlalchemy.exc import IntegrityError
+from userbot import (COUNT_PM, CMD_HELP, BOTLOG, BOTLOG_CHATID, PM_AUTO_BAN,LASTMSG, LOGS)
+from userbot import CMD_HELP, ALIVE_NAME, PM_MESSAGE, KYNE_NAME, KYNE_MSG, ORI_MSG
+KYNE_NNAME = str(KYNE_NAME) if KYNE_NAME else str(KYNE_MSG)
+BLOCK_MMSG = str(BLOCK_MESSAGE) if BLOCK_MESSAGE else str(BLOCK_MSG)
+AFK_MMSG = str(AFK_MESSAGE) if AFK_MESSAGE else str(AFK_MSG)
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+PM_MESSAGE = str(PM_MESSAGE) if PM_MESSAGE else str(ORI_MSG)
+# ========================= CONSTANTS ============================
+UNAPPROVED_MSG = (
+   f"`{JAVES_NNAME}:`**{PM_MESSAGE}**")
+    
+
+@kyne3301(incoming=True, disable_edited=True, disable_errors=True)
+async def permitpm(event):
+    """ Prohibits people from PMing you without approval. \
+        Will block retarded nibbas automatically. """
+    if PM_AUTO_BAN:
+        self_user = await event.client.get_me()
+        if event.is_private and event.chat_id != 710844948 and event.chat_id != self_user.id and not (
+                await event.get_sender()).bot:
+            try:
+                from userbot.modules.sql_helper.pm_permit_sql import is_approved
+                from userbot.modules.sql_helper.globals import gvarstatus
+            except AttributeError:
+                return
+            apprv = is_approved(event.chat_id)
+            notifsoff = gvarstatus("NOTIF_OFF")
+
+            # This part basically is a sanity check
+            # If the message that sent before is Unapproved Message
+            # then stop sending it again to prevent FloodHit
+            if not apprv and event.text != UNAPPROVED_MSG:
+                if event.chat_id in LASTMSG:
+                    prevmsg = LASTMSG[event.chat_id]
+                    # If the message doesn't same as previous one
+                    # Send the Unapproved Message again
+                    if event.text != prevmsg:
+                        async for message in event.client.iter_messages(
+                                event.chat_id,
+                                from_user='me',
+                                search=UNAPPROVED_MSG):
+                            await message.delete()
+                        
+                    LASTMSG.update({event.chat_id: event.text})
+                else:
+                    await event.reply(UNAPPROVED_MSG)
+                    LASTMSG.update({event.chat_id: event.text})
+
+                if notifsoff:
+                    await event.client.send_read_acknowledge(event.chat_id)
+                if event.chat_id not in COUNT_PM:
+                    COUNT_PM.update({event.chat_id: 1})
+                else:
+                    COUNT_PM[event.chat_id] = COUNT_PM[event.chat_id] + 1
+
+                if COUNT_PM[event.chat_id] == 3:
+                    await event.respond(
+                             f"`{JAVES_NNAME}`: ** Dont spam my master's pm this is your last warning!!**")
+                if COUNT_PM[event.chat_id] > 4:
+                    await event.respond(
+                             f"`{JAVES_NNAME}`: ** {BLOCK_MMSG} **")        
+                
+
+                    try:
+                        del COUNT_PM[event.chat_id]
+                        del LASTMSG[event.chat_id]
+                    except KeyError:
+                        if BOTLOG:
+                            await event.client.send_message(
+                                BOTLOG_CHATID,
+                                "Count PM is seemingly going retard, plis restart bot!",
+                            )
+                        LOGS.info("CountPM wen't rarted boi")
+                        return
+
+                    await event.client(BlockRequest(event.chat_id))
+                    await event.client(ReportSpamRequest(peer=event.chat_id))
+
+                    if BOTLOG:
+                        name = await event.client.get_entity(event.chat_id)
+                        name0 = str(name.first_name)
+                        await event.client.send_message(
+                            BOTLOG_CHATID,
+                            "[" + name0 + "](tg://user?id=" +
+                            str(event.chat_id) + ")" +
+                            " blocked  for spam your PM",
+                        )
+
+
+@javes05(disable_edited=True, outgoing=True, disable_errors=True)
+async def auto_accept(event):
+    """ Will approve automatically if you texted them first. """
+    if not PM_AUTO_BAN:
+        return
+    self_user = await event.client.get_me()
+    if event.is_private and event.chat_id != 777000 and event.chat_id != self_user.id and not (
+            await event.get_sender()).bot:
+        try:
+            from userbot.modules.sql_helper.pm_permit_sql import is_approved
+            from userbot.modules.sql_helper.pm_permit_sql import approve
+        except AttributeError:
+            return
+
+        chat = await event.get_chat()
+        if isinstance(chat, User):
+            if is_approved(event.chat_id) or chat.bot:
+                return
+            async for message in event.client.iter_messages(event.chat_id,
+                                                            reverse=True,
+                                                            limit=1):
+                if message.message is not UNAPPROVED_MSG and message.from_id == self_user.id:
+                    try:
+                        approve(event.chat_id)
+                    except IntegrityError:
+                        return
+
+                if is_approved(event.chat_id) and BOTLOG:
+                    await event.client.send_message(
+                        BOTLOG_CHATID,
+                        "#AUTO-APPROVED\n" + "User: " +
+                        f"[{chat.first_name}](tg://user?id={chat.id})",
+                    )
+
+
+@javes05(outgoing=True, pattern="^\!notifoff$")
+async def notifoff(noff_event):
+    """ For .notifoff command, stop getting notifications from unapproved PMs. """
+    try:
+        from userbot.modules.sql_helper.globals import addgvar
+    except AttributeError:
+        await noff_event.edit("`Running on Non-SQL mode!`")
+        return
+    addgvar("NOTIF_OFF", True)
+    await noff_event.edit("`Notifications from unapproved PM's are silenced!`")
+
+
+@javes05(outgoing=True, pattern="^\!notifon$")
+async def notifon(non_event):
+    """ For .notifoff command, get notifications from unapproved PMs. """
+    try:
+        from userbot.modules.sql_helper.globals import delgvar
+    except AttributeError:
+        await non_event.edit("`Running on Non-SQL mode!`")
+        return
+    delgvar("NOTIF_OFF")
+    await non_event.edit("`Notifications from unapproved PM's unmuted!`")
+
+
+@javes05(outgoing=True, pattern="^\!allow$")
+async def approvepm(apprvpm):
+    """ For .approve command, give someone the permissions to PM you. """
+    try:
+        from userbot.modules.sql_helper.pm_permit_sql import approve
+    except AttributeError:
+        await apprvpm.edit("`Running `")
+        return
+
+    if apprvpm.reply_to_msg_id:
