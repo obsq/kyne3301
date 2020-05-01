d6378d3bff0e94068f8dfb568e7975c17a354c73
obsq <63944348+obsq@users.noreply.github.com>
2020-05-01 12:03:00 +0530

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -211,3 +211,322 @@ async def kang(args):
                 # Ensure user doesn't get spamming notifications
                 await bot.send_read_acknowledge(conv.chat_id)
         else:
+            await args.edit(f"`{KYNE_NNAME}`: **Brewing a new Pack...**")
+            async with bot.conversation('Stickers') as conv:
+                await conv.send_message(cmd)
+                await conv.get_response()
+                # Ensure user doesn't get spamming notifications
+                await bot.send_read_acknowledge(conv.chat_id)
+                await conv.send_message(packnick)
+                await conv.get_response()
+                # Ensure user doesn't get spamming notifications
+                await bot.send_read_acknowledge(conv.chat_id)
+                if is_anim:
+                    await conv.send_file('AnimatedSticker.tgs')
+                    remove('AnimatedSticker.tgs')
+                else:
+                    file.seek(0)
+                    await conv.send_file(file, force_document=True)
+                rsp = await conv.get_response()
+                if "Sorry, the file type is invalid." in rsp.text:
+                    await args.edit(
+                        "`Failed to add sticker, use` @Stickers `bot to add the sticker manually.`"
+                    )
+                    return
+                await conv.send_message(emoji)
+                # Ensure user doesn't get spamming notifications
+                await bot.send_read_acknowledge(conv.chat_id)
+                await conv.get_response()
+                await conv.send_message("/publish")
+                if is_anim:
+                    await conv.get_response()
+                    await conv.send_message(f"<{packnick}>")
+                # Ensure user doesn't get spamming notifications
+                await conv.get_response()
+                await bot.send_read_acknowledge(conv.chat_id)
+                await conv.send_message("/skip")
+                # Ensure user doesn't get spamming notifications
+                await bot.send_read_acknowledge(conv.chat_id)
+                await conv.get_response()
+                await conv.send_message(packname)
+                # Ensure user doesn't get spamming notifications
+                await bot.send_read_acknowledge(conv.chat_id)
+                await conv.get_response()
+                # Ensure user doesn't get spamming notifications
+                await bot.send_read_acknowledge(conv.chat_id)
+
+        await args.edit(f"`{KYNE_NNAME}`: **Sticker kanged successfully!**\
+            \nPack can be found [here](t.me/addstickers/{packname})",
+                        parse_mode='md')
+
+
+async def resize_photo(photo):
+    """ Resize the given photo to 512x512 """
+    image = Image.open(photo)
+    maxsize = (512, 512)
+    if (image.width and image.height) < 512:
+        size1 = image.width
+        size2 = image.height
+        if image.width > image.height:
+            scale = 512 / size1
+            size1new = 512
+            size2new = size2 * scale
+        else:
+            scale = 512 / size2
+            size1new = size1 * scale
+            size2new = 512
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        image = image.resize(sizenew)
+    else:
+        image.thumbnail(maxsize)
+
+    return image
+
+
+
+
+
+
+
+
+@kyne3301(outgoing=True, disable_errors=True, pattern="^!ss2(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit(f"`{KYNE_NNAME}`: **Reply to a Photo**")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit(f"`{KYNE_NNAME}`: **Reply to a photo**")
+       return
+    chat = "@BuildStickerBot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit(f"`{KYNE_NNAME}`: **Reply to actual users message.**")
+       return
+    await event.edit(f"`{KYNE_NNAME}`: **processing Img to Sticker.......**")
+    async with bot.conversation(chat) as conv:
+          try:                
+              await conv.send_message("/start")
+              response2 = await conv.get_response()     
+              if response2.text.startswith("🔸"):      
+                 await asyncio.sleep(0.3)                 
+                 response = conv.wait_event(events.NewMessage(incoming=True,from_users=164977173))
+                 await bot.forward_messages(chat, reply_message)
+                 response = await response 
+                 #await event.reply(f"`{KYNE_NNAME}`: **Converted @{sender.username}'s photo to Sticker Sucessfully**")
+                 if response.text.startswith("🔸"):               
+                    await event.edit(f"`{KYNE_NNAME}`: **Sorry, Failed to decode this img \n try use alternative way !ss3**")
+                 else: 
+          	       await bot.send_file(event.chat_id, response.message.media)      
+              else:
+              	await event.edit(f"`{KYNE_NNAME}`: **Sorry, currently this command in maintaince..... please try later \nOr use alternative way !ss3  **")             	      
+          except YouBlockedUserError: 
+              await event.reply(f"`{KYNE_NNAME}`: **Please unblock @BuildStickerBot and try again**")
+              
+          
+          		
+
+
+
+@kyne3301(outgoing=True, pattern="^\!stickerinfo$")
+async def get_pack_info(event):
+    if not event.is_reply:
+        await event.edit(f"`{KYNE_NNAME}`: **Reply to a sticker to get the pack details*")
+        return
+
+    rep_msg = await event.get_reply_message()
+    if not rep_msg.document:
+        await event.edit(f"`{KYNE_NNAME}`: **Reply to a sticker to get the pack details**")
+        return
+
+    try:
+        stickerset_attr = rep_msg.document.attributes[1]
+        await event.edit(
+            "`Fetching details of the sticker pack, please wait..`")
+    except BaseException:
+        await event.edit("`This is not a sticker. Reply to a sticker.`")
+        return
+
+    if not isinstance(stickerset_attr, DocumentAttributeSticker):
+        await event.edit(f"`{KYNE_NNAME}`: **This is not a sticker. Reply to a sticker.**")
+        return
+
+    get_stickerset = await bot(
+        GetStickerSetRequest(
+            InputStickerSetID(
+                id=stickerset_attr.stickerset.id,
+                access_hash=stickerset_attr.stickerset.access_hash)))
+    pack_emojis = []
+    for document_sticker in get_stickerset.packs:
+        if document_sticker.emoticon not in pack_emojis:
+            pack_emojis.append(document_sticker.emoticon)
+
+    OUTPUT = f"**Sticker Title:** `{get_stickerset.set.title}\n`" \
+        f"**Sticker Short Name:** `{get_stickerset.set.short_name}`\n" \
+        f"**Official:** `{get_stickerset.set.official}`\n" \
+        f"**Archived:** `{get_stickerset.set.archived}`\n" \
+        f"**Stickers In Pack:** `{len(get_stickerset.packs)}`\n" \
+        f"**Emojis In Pack:**\n{' '.join(pack_emojis)}"
+
+    await event.edit(OUTPUT)
+
+
+
+@kyne3301(outgoing=True, pattern="^!ss(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit(f"`{KYNE_NNAME}`: **Reply to any user message.**")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit(f"`{KYNE_NNAME}`: **Reply to text message**")
+       return
+    chat = "@QuotLyBot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit(f"`{KYNE_NNAME}`: **Reply to actual users message**")
+       return
+    await event.edit(f"`{KYNE_NNAME}`: ** Processing user chat to Sticker **")
+    async with bot.conversation(chat) as conv:
+          try:                
+              await conv.send_message("/start")
+              response2 = await conv.get_response()     
+              if response2.text.startswith("**Hi!**"): 
+                 await asyncio.sleep(0.3)
+                 #await event.delete()
+                 response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))                 
+                 await bot.forward_messages(chat, reply_message)
+                 response = await response 
+                 #await event.reply(f"`{JAVES_NNAME}`: **Converted @{sender.username}'s** ` {reply_message.message} `**message to Sticker Sucessfully**")
+                 if response.text.startswith("Command"):
+                    await event.edit(f"`{JAVES_NNAME}`: **Sorry, I Failed to convert it**")
+                 else:                        
+          	         await bot.send_file(event.chat_id, response.message.media)                               
+              else:
+              	await event.edit(f"`{JAVES_NNAME}`: **Sorry, currently this command in maintaince..... please try later **")             	      
+          except YouBlockedUserError:          	
+              await event.reply(f"`{JAVES_NNAME}`: **Please unblock @QuotLyBot and try again**")
+              
+          
+          		
+ 
+
+
+
+
+
+
+@bot.on(events.NewMessage(outgoing=True, pattern="!text (?:(.*?) \| )?(.*)"))
+@bot.on(events.MessageEdited(outgoing=True, pattern="!text (?:(.*?) \| )?(.*)"))
+async def sticklet(event):
+    R = random.randint(0,256)
+    G = random.randint(0,256)
+    B = random.randint(0,256)
+    reply_message = event.message
+    # get the input text
+    # the text on which we would like to do the magic on
+    font_file_name = event.pattern_match.group(1)
+    if not font_file_name:
+        font_file_name = ""
+    sticktext = event.pattern_match.group(2)
+    if not sticktext and event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        sticktext = reply_message.message
+    elif not sticktext:
+        await event.edit(f"`{JAVES_NNAME}`: **need something **")
+        return
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+    # delete the userbot command,
+    # i don't know why this is required
+    await event.delete()
+    # https://docs.python.org/3/library/textwrap.html#textwrap.wrap
+    sticktext = textwrap.wrap(sticktext, width=10)
+    # converts back the list to a string
+    sticktext = '\n'.join(sticktext)
+    image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(image)
+    fontsize = 230
+    FONT_FILE = await get_font_file(event.client, "@FontRes", font_file_name)
+    font = ImageFont.truetype(FONT_FILE, size=fontsize)
+    while draw.multiline_textsize(sticktext, font=font) > (512, 512):
+        fontsize -= 3
+        font = ImageFont.truetype(FONT_FILE, size=fontsize)
+    width, height = draw.multiline_textsize(sticktext, font=font)
+    draw.multiline_text(((512-width)/2,(512-height)/2), sticktext, font=font, fill=(R, G, B))
+    image_stream = io.BytesIO()
+    image_stream.name = "javes.webp"
+    image.save(image_stream, "WebP")
+    image_stream.seek(0)
+    # finally, reply the sticker
+    await event.client.send_file(event.chat_id, image_stream, caption="Javes", reply_to=event.message.reply_to_msg_id)
+    # cleanup
+    try:
+        os.remove(FONT_FILE)
+    except:
+        pass
+async def get_font_file(client, channel_id, search_kw=""):
+    # first get the font messages
+    font_file_message_s = await client.get_messages(
+        entity=channel_id,
+        filter=InputMessagesFilterDocument,
+        # this might cause FLOOD WAIT,
+        # if used too many times
+        limit=None,
+        search=search_kw
+    )
+    # get a random font from the list of fonts
+    # https://docs.python.org/3/library/random.html#random.choice
+    font_file_message = random.choice(font_file_message_s)
+    # download and return the file path
+    return await client.download_media(font_file_message)
+
+
+
+
+@javes05(outgoing=True, disable_errors=True, pattern="^!ss3(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit(f"`{JAVES_NNAME}`: **Reply to a Photo/sticker**")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit(f"`{JAVES_NNAME}`: **Reply to a photo/sticker**")
+       return
+    chat = "@Stickerdownloadbot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit(f"`{JAVES_NNAME}`: **Reply to actual users message.**")
+       return
+    await event.edit(f"`{JAVES_NNAME}`: **converting .........**")
+    async with bot.conversation(chat) as conv:
+          try:                
+              await conv.send_message("/start")
+              response2 = await conv.get_response()     
+              if response2.text.startswith("Hi"):      
+                 await asyncio.sleep(0.3)
+                 #await event.delete()
+                 response = conv.wait_event(events.NewMessage(incoming=True,from_users=220255550))
+                 await bot.forward_messages(chat, reply_message)
+                 response = await response 
+                 #await event.reply(f"`{JAVES_NNAME}`: **Converted @{sender.username}'s photo Sucessfully**")
+                 if response.text.startswith("I"):               
+                    await event.edit(f"`{JAVES_NNAME}`: **Sorry, Failed to decode this \n try use alternative way !ss2**")
+                 else:
+          	        await bot.send_file(event.chat_id, response.message.media)      
+                 await conv.get_response()
+                 rk1 = await conv.get_response()
+                 await bot.send_file(event.chat_id, rk1)                 
+              else:
+              	await event.edit(f"`{JAVES_NNAME}`: **Sorry, currently this command in maintaince..... please try later \nOr use alternative way !ss2  **")             	      
+          except YouBlockedUserError: 
+              await event.reply(f"`{JAVES_NNAME}`: **Please unblock @BuildStickerBot and try again**")
+              
