784e1cea147f374fb0d2320744ef2dac3556ab3e
obsq <63944348+obsq@users.noreply.github.com>
2020-05-10 21:53:50 +0530

    Update fun.py

--- userbot/modules/fun.py
+++ userbot/modules/fun.py
@@ -878,3 +878,201 @@ async def copypasta(cp_e):
     reply_text += choice(EMOJIS)
     await cp_e.edit(reply_text)
 
+
+@kyne3301(outgoing=True, pattern="^!vapor(?: |$)(.*)")
+async def vapor(vpr):
+    """ Vaporize everything! """
+    reply_text = list()
+    textx = await vpr.get_reply_message()
+    message = vpr.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await vpr.edit("`Ｇｉｖｅ ｓｏｍｅ ｔｅｘｔ ｆｏｒ ｖａｐｏｒ！`")
+        return
+
+    for charac in message:
+        if 0x21 <= ord(charac) <= 0x7F:
+            reply_text.append(chr(ord(charac) + 0xFEE0))
+        elif ord(charac) == 0x20:
+            reply_text.append(chr(0x3000))
+        else:
+            reply_text.append(charac)
+
+    await vpr.edit("".join(reply_text))
+
+
+@kyne3301(outgoing=True, pattern="^!str(?: |$)(.*)")
+async def stretch(stret):
+    """ Stretch it."""
+    textx = await stret.get_reply_message()
+    message = stret.text
+    message = stret.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await stret.edit("`GiiiiiiiB sooooooomeeeeeee teeeeeeext!`")
+        return
+
+    count = randint(3, 10)
+    reply_text = sub(r"([aeiouAEIOUａｅｉｏｕＡＥＩＯＵаеиоуюяыэё])", (r"\1" * count),
+                     message)
+    await stret.edit(reply_text)
+
+
+@kyne3301(outgoing=True, pattern="^!style(?: |$)(.*)")
+async def zal(zgfy):
+    """ Invoke the feeling of chaos. """
+    reply_text = list()
+    textx = await zgfy.get_reply_message()
+    message = zgfy.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await zgfy.edit(
+            "`gͫ ̆ i̛ ̺ v͇̆ ȅͅ   a̢ͦ   s̴̪ c̸̢ ä̸ rͩͣ y͖͞   t̨͚ é̠ x̢͖  t͔͛`"
+        )
+        return
+
+    for charac in message:
+        if not charac.isalpha():
+            reply_text.append(charac)
+            continue
+
+        for _ in range(0, 3):
+            textz = randint(0, 2)
+
+            if textz == 0:
+                charac = charac.strip() + \
+                    choice(ZALG_LIST[0]).strip()
+            elif textz == 1:
+                charac = charac.strip() + \
+                    choice(ZALG_LIST[1]).strip()
+            else:
+                charac = charac.strip() + \
+                    choice(ZALG_LIST[2]).strip()
+
+        reply_text.append(charac)
+
+    await zgfy.edit("".join(reply_text))
+
+
+@kyne3301(outgoing=True, pattern="^!hi$")
+async def hoi(hello):
+    """ Greet everyone! """
+    await hello.edit(choice(HELLOSTR))
+
+
+@kyne3301(outgoing=True, pattern="^!owo(?: |$)(.*)")
+async def faces(owo):
+    """ UwU """
+    textx = await owo.get_reply_message()
+    message = owo.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await owo.edit("` UwU no text given! `")
+        return
+
+    reply_text = sub(r"(r|l)", "w", message)
+    reply_text = sub(r"(R|L)", "W", reply_text)
+    reply_text = sub(r"n([aeiou])", r"ny\1", reply_text)
+    reply_text = sub(r"N([aeiouAEIOU])", r"Ny\1", reply_text)
+    reply_text = sub(r"\!+", " " + choice(UWUS), reply_text)
+    reply_text = reply_text.replace("ove", "uv")
+    reply_text += " " + choice(UWUS)
+    await owo.edit(reply_text)
+
+
+@kyne3301(outgoing=True, pattern="^!react$")
+async def react_meme(react):
+    """ Make your userbot react to everything. """
+    await react.edit(choice(FACEREACTS))
+
+
+@kyne3301(outgoing=True, pattern="^!iwi(?: |$)(.*)")
+async def faces(siwis):
+    """ IwI """
+    textx = await siwis.get_reply_message()
+    message = siwis.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await siwis.edit("` IwI no text given! `")
+        return
+
+    reply_text = sub(r"(a|i|u|e|o)", "i", message)
+    reply_text = sub(r"(A|I|U|E|O)", "I", reply_text)
+    reply_text = sub(r"\!+", " " + choice(IWIS), reply_text)
+    reply_text += " " + choice(IWIS)
+    await siwis.edit(reply_text)
+
+
+@kyne3301(outgoing=True, pattern="^!shg$")
+async def shrugger(shg):
+    r""" ¯\_(ツ)_/¯ """
+    await shg.edit(choice(SHGS))
+
+
+
+
+
+
+@kyne3301(outgoing=True, pattern="^Oof$")
+async def Oof(e):
+    t = "Oof"
+    for j in range(15):
+        t = t[:-1] + "of"
+        await e.edit(t)
+
+
+@kyne3301(outgoing=True, pattern="^!kill$")
+async def iqless(e):
+    await e.edit("User Killed Sucessfully  ▀̿ ̿Ĺ̯̿̿▀̿ ̿")      
+
+
+@kyne3301(outgoing=True, pattern="^!corona$")
+async def iqless(e):
+    await e.edit("Antivirus scan was completed \n⚠️ Warning! This  donkey has Corona Virus")      
+
+   
+
+
+                     
+@kyne3301(outgoing=True, pattern="^!bye$")
+async def iqless(e):
+    await e.edit("Kek thx bye")       
+                      
+
+@kyne3301(outgoing=True, pattern="^!moon$")
+async def moon(event):
+    deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+    try:
+        for x in range(32):
+            await sleep(0.1)
+            await event.edit("".join(deq))
+            deq.rotate(1)
+    except BaseException:
+        return
+
+                   
+@kyne3301(outgoing=True, pattern="^!earth$")
+async def earth(event):
+    deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
+    try:
+        for x in range(32):
+            await sleep(0.1)
+            await event.edit("".join(deq))
+            deq.rotate(1)
+    except BaseException:
+        return                              
