2637108ec1e09f610b4ef04ff3798c0a35b0eb05
obsq <63944348+obsq@users.noreply.github.com>
2020-05-11 19:17:33 +0530

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -187,7 +187,6 @@ from telethon import events
 from userbot import bot, BOTLOG_CHATID, LOGSPAMMER
 
 
-
 def kyne3301(**args):
     """ Register a new event. """
     stack = inspect.stack()
@@ -261,6 +260,114 @@ def kyne3301(**args):
             try:
                 await func(check)
 
+            # Thanks to @kandnub for this HACK.
+            # Raise StopPropagation to Raise StopPropagation
+            # This needed for AFK to working properly
+
+            except events.StopPropagation:
+                raise events.StopPropagation
+            # This is a gay exception and must be passed out. So that it doesnt spam chats
+            except KeyboardInterrupt:
+                pass
+            except BaseException:
+
+                # Check if we have to disable it.
+                # If not silence the log spam on the console,
+                # with a dumb except.
+
+                if not disable_errors:
+                    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+                    text = "**KYNE ERROR REPORT**\n"
+                    text += "Send this to @obsquriel if you cant find issue\n"
+
+                    ftext = "========== DISCLAIMER =========="
+                    ftext += "\nThis file uploaded only logchat,"                
+                    ftext += "\nreport to admin this error if you cant find any issue"
+                    ftext += "\n---------------------------------\n"
+                    ftext += "================================\n\n"
+                    ftext += "--------BEGIN LOG--------\n"
+                    ftext += "\nDate: " + date
+                    ftext += "\nChat ID: " + str(check.chat_id)
+                    ftext += "\nSender ID: " + str(check.sender_id)
+                    ftext += "\n\nEvent Trigger:\n"
+                    ftext += str(check.text)
+                    ftext += "\n\nTraceback info:\n"
+                    ftext += str(format_exc())
+                    ftext += "\n\nError text:\n"
+                    ftext += str(sys.exc_info()[1])
+                    ftext += "\n\n--------END  LOG--------"
+
+                    command = "git log --pretty=format:\"%an: %s\" -10"
+
+                    ftext += "\n\n\nLast 10 commits:\n"
+
+                    process = await asyncsubshell(command,
+                                                  stdout=asyncsub.PIPE,
+                                                  stderr=asyncsub.PIPE)
+                    stdout, stderr = await process.communicate()
+                    result = str(stdout.decode().strip()) \
+                        + str(stderr.decode().strip())
+
+                    ftext += result
+
+                    file = open("kyne_error.log", "w+")
+                    file.write(ftext)
+                    file.close()
+
+               
+                    
+                    await check.client.send_file(send_to,
+                                                 "kyne_error.log",
+                                                 caption=text)
+                    remove("kyne_error.log")
+            else:
+                pass
+                
+        if not disable_edited:
+            bot.add_event_handler(wrapper, events.MessageEdited(**args))
+        bot.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+    
+def kynee(**args):
+    """ Register a new event. """
+    pattern = args.get('pattern', None)
+    disable_edited = args.get('disable_edited', False)
+    groups_only = args.get('groups_only', False)
+    trigger_on_fwd = args.get('trigger_on_fwd', False)
+    disable_errors = args.get('disable_errors', False)
+    
+    
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+
+    if "disable_edited" in args:
+        del args['disable_edited']
+
+    if "groups_only" in args:
+        del args['groups_only']
+
+    if "disable_errors" in args:
+        del args['disable_errors']
+
+    if "trigger_on_fwd" in args:
+        del args['trigger_on_fwd']
+
+    def decorator(func):
+        async def wrapper(check):
+            if LOGSPAMMER:
+                send_to = BOTLOG_CHATID
+
+            if not trigger_on_fwd and check.fwd_from:
+                return
+
+            if groups_only and not check.is_group:
+                await check.respond("`I don't think this is a group.`")
+                return            
+            try:
+                await func(check)
 
             # Thanks to @kandnub for this HACK.
             # Raise StopPropagation to Raise StopPropagation
@@ -280,8 +387,8 @@ def kyne3301(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**kyne ERROR REPORT**\n"
-                    text += "Send this to @obsquriel if you cant find issue\n"
+                    text = "**KYNE ERROR REPORT**\n"
+                    text += "Send this to @rekcah05 if you cant find issue\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded only logchat,"                
@@ -359,8 +466,8 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
         progress_str = "[{0}{1}]\nProgress: {2}%\n".format(
-            ''.join(["?" for i in range(math.floor(percentage / 5))]),
-            ''.join(["?" for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["-" for i in range(math.floor(percentage / 5))]),
+            ''.join(["-" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
         tmp = progress_str + \
             "{0} of {1}\nETA: {2}".format(
@@ -409,6 +516,8 @@ class Loader():
     def __init__(self, func=None, **args):
         self.Var = Var
         bot.add_event_handler(func, events.NewMessage(**args))
+
+
 
 
 
