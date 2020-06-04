36372e21d719e302735535685f83c0450ebe844f
OႦʂϙ <63944348+obsq@users.noreply.github.com>
2020-06-04 17:17:58 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -37,40 +37,26 @@ async def install(event):
                 await event.edit("Installed module `{}`".format(os.path.basename(downloaded_file_name)))
             else:
                 os.remove(downloaded_file_name)
-                await event.edit("Errors! Cannot install this plugin.")
-        except Exception as e:  # pylint:disable=C0103,W0703
-            await event.edit(str(e))
-            os.remove(downloaded_file_name)
-    await asyncio.sleep(DELETE_TIMEOUT)
-    await event.delete()
-
-
-
-@command(pattern="^!sqlinstall", outgoing=True)
-async def install(event):
-    if event.fwd_from:
-        return
-    if event.reply_to_msg_id:
-        try:
-            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
-                await event.get_reply_message(),
-                "/root/userbot/userbot/modules/sql_helper"  # pylint:disable=E0602
-            )
-            if "(" not in downloaded_file_name:
-                path1 = Path(downloaded_file_name)
-                shortname = path1.stem
-                load_module(shortname.replace(".py", ""))
-                await event.edit("Installed sql module `{}`".format(os.path.basename(downloaded_file_name)))
-            else:
-                os.remove(downloaded_file_name)
                 await event.edit(f"`{JAVES_NNAME}`: **Error, module already installed or unknown formet**")
         except Exception as e:  # pylint:disable=C0103,W0703
             await event.edit(str(e))
             os.remove(downloaded_file_name)
-    await asyncio.sleep(DELETE_TIMEOUT)
-    await event.delete()
+    
 
-
+@command(pattern="^!load (?P<shortname>\w+)$", outgoing=True)
+async def load(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
+        try:
+            remove_plugin(shortname)
+        except:
+            pass
+        load_module(shortname)
+        await event.edit(f"Successfully loaded {shortname}")
+    except Exception as e:
+        await event.edit(f"Could not load {shortname} because of the following error.\n{str(e)}")
 
 
 @command(pattern="^!send (?P<shortname>\w+)$", outgoing=True)
@@ -94,6 +80,7 @@ async def send(event):
     await asyncio.sleep(DELETE_TIMEOUT)
     await event.delete()
 
+
 @command(pattern="^!unload (?P<shortname>\w+)$", outgoing=True)
 async def unload(event):
     if event.fwd_from:
@@ -104,18 +91,3 @@ async def unload(event):
         await event.edit(f"Unloaded {shortname} successfully")
     except Exception as e:
         await event.edit("Successfully unload {shortname}\n{}".format(shortname, str(e)))
-
-@command(pattern="^!load (?P<shortname>\w+)$", outgoing=True)
-async def load(event):
-    if event.fwd_from:
-        return
-    shortname = event.pattern_match["shortname"]
-    try:
-        try:
-            remove_plugin(shortname)
-        except:
-            pass
-        load_module(shortname)
-        await event.edit(f"Successfully loaded {shortname}")
-    except Exception as e:
-        await event.edit(f"Could not load {shortname} because of the following error.\n{str(e)}")
