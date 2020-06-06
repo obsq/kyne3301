517a5449fe0afa7f34711b27541653858479d706
OႦʂϙ <63944348+obsq@users.noreply.github.com>
2020-06-06 10:53:12 +0530

    Update extra.py

--- userbot/modules/extra.py
+++ userbot/modules/extra.py
@@ -3575,7 +3575,7 @@ async def autopic(event):
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((150, 0), current_time, font=fnt, fill=(50, 0, 20))
+        drawn_text.text((95, 250), current_time, font=fnt, fill=(255, 255, 255))
         img.save(photo)
         file = await bot.upload_file(photo)  # pylint:disable=E0602
         await event.reply(f"**{KYNE_NAME}**:` successfully set profile picture \nSleeping 60s.......`")
